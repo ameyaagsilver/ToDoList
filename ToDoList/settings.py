10960b98365c98b4992d6ade08d7e97784e8b4fe
ameyaagsilver <ameyaagsilver@gmail.com>
2021-12-06 23:06:53 +0530

    DEBUG = False now

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o&yutfozaxh22t%-6z4%pe*j^8oxytf6r6(@gx45imla1p4dah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
